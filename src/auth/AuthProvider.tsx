76fe54f098b178bca9f6c135eac3266a96ff204e
Ramin <raminxanifmuslim@gmail.com>
2025-02-04 17:20:31 +0400

    change env file

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -5,15 +5,11 @@ import {
 } from "react-oauth2-code-pkce";
 import { useContext } from "react";
 
-const BASE_URL = "http://localhost:5173";
-const AUTH_URL =
-  "https://app-vms-identity-test-cqfjgpfmdpfhd8cp.germanywestcentral-01.azurewebsites.net";
-
 const authConfig: TAuthConfig = {
   clientId: import.meta.env.VITE_OAUTH_CLIENT_ID,
-  authorizationEndpoint: `${AUTH_URL}/connect/authorize`,
-  tokenEndpoint: `${AUTH_URL}/connect/token`,
-  redirectUri: `${BASE_URL}`,
+  authorizationEndpoint: `${import.meta.env.VITE_AUTH_URL}/connect/authorize`,
+  tokenEndpoint: `${import.meta.env.VITE_AUTH_URL}/connect/token`,
+  redirectUri: `${import.meta.env.VITE_HOST_URL}`,
   scope: import.meta.env.VITE_OAUTH_SCOPE,
 };
 
