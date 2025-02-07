afba6bf941f5e0e94a39a0a1f04ee506cad4b1fb
Ramin <raminxanifmuslim@gmail.com>
2025-02-07 17:07:55 +0400

    fix bag css

--- src/layouts/document-layout/DocumentManagementLayout.tsx
+++ src/layouts/document-layout/DocumentManagementLayout.tsx
@@ -25,39 +25,37 @@ const DocumentManagementLayout = () => {
   };
 
   return (
-    <div className="h-screen bg-white">
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel
-          defaultSize={defaultSize}
-          minSize={!isOpenSidebar ? 15 : 0}
-          onResize={handleResize}
-          className={classNames({
-            "hidden h-full md:block": true,
-            "!hidden": !isOpenSidebar,
-          })}
-        >
-          {/* sidebar */}
-          <AnimatePresence>
-            <motion.aside
-              initial={{ minWidth: isOpenSidebar ? 0 : 300 }}
-              animate={{
-                minWidth: isOpenSidebar ? 300 : 0,
-                transition: { duration: 0.3 },
-              }}
-              className="hidden shrink-0 md:block"
-            >
-              <Sidebar />
-            </motion.aside>
-          </AnimatePresence>
-        </ResizablePanel>
-        <ResizableHandle withHandle={isOpenSidebar} />
-        <ResizablePanel>
-          <div className="h-full overflow-y-auto">
-            <Outlet />
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel
+        defaultSize={defaultSize}
+        minSize={!isOpenSidebar ? 15 : 0}
+        onResize={handleResize}
+        className={classNames({
+          "hidden h-full bg-white md:block": true,
+          "!hidden": !isOpenSidebar,
+        })}
+      >
+        {/* sidebar */}
+        <AnimatePresence>
+          <motion.aside
+            initial={{ minWidth: isOpenSidebar ? 0 : 300 }}
+            animate={{
+              minWidth: isOpenSidebar ? 300 : 0,
+              transition: { duration: 0.3 },
+            }}
+            className="hidden shrink-0 md:block"
+          >
+            <Sidebar />
+          </motion.aside>
+        </AnimatePresence>
+      </ResizablePanel>
+      <ResizableHandle withHandle={isOpenSidebar} />
+      <ResizablePanel>
+        <div className="h-full overflow-y-auto">
+          <Outlet />
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 };
 
